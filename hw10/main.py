c71e1a1b4120619a1379f1c3ab943c3aa9f6ccd0
jArkee1337 <jArkee@yandex.ru>
2022-03-11 13:40:39 +0800

    hw10

--- hw10/main.py
+++ hw10/main.py
@@ -11,30 +11,44 @@ class Name(Field):
 
 
 class Phone(Field):
-    pass
+
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+
+    def __str__(self):
+        return f"{self.phone_number}"
 
 
 class Record:
-    name = Name("Vladimir")
 
-    def __init__(self):
+    def __init__(self, name):
         self.phones = []
+        self.name = Name(name)
 
     def add_func(self, phone_number):
-        self.phones.append(phone_number)
+        self.phones.append(Phone(phone_number))
+
         return self.phones
 
     def change_func(self, phone_number, new_phone_number):
-        if phone_number in self.phones:
-            self.phones.remove(phone_number)
-        self.phones.append(new_phone_number)
+
+        for i in self.phones[:]:
+            if str(i) == phone_number:
+                self.phones.remove(i)
+        self.phones.insert(0, Phone(new_phone_number))
 
     def remove_func(self, phone_number):
-        if phone_number in self.phones:
-            self.phones.remove(phone_number)
+        for i in self.phones:
+            if str(i) == phone_number:
+                self.phones.remove(i)
 
 
 class AddressBook(UserDict):
 
-    def add_record(self, key, value):
-        self.data[key] = value
+    def add_record(self, record_name_value, record):
+        self.data[record_name_value] = record
+
+    def remove_record(self, record_name_value):
+        self.data.pop(record_name_value)
+
+
